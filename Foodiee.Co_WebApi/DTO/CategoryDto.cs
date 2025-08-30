719c6a3cd456b0bd15660b8d0a54c81c0f5caf98
SREE VARSHA LAGISHETTI <sreevarsha.lagishetti11@gmail.com>
2025-08-31 00:23:13 +0530

    Authentication Done

--- Foodiee.Co_WebApi/DTO/CategoryDto.cs
+++ Foodiee.Co_WebApi/DTO/CategoryDto.cs
@@ -1,10 +1,8 @@
-﻿using System.Text.Json.Serialization;
 
 namespace Foodiee.Co_WebApi.DTO
 {
     public class CategoryDto
     {
-        [JsonPropertyName("id")]
         public int Id { get; set; }
         [JsonPropertyName("name")]
         public string Name { get; set; }
